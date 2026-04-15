4d35e633ffb16a95426d3276870a8b2abed805c7
Miguel Ordóñez <mordonez@duck.com>
2026-04-15 10:34:25 +0200

    feat(liferay): add sanitized error factory and integrate key commands

---
+++ src/features/liferay/errors/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Liferay errors module: unified, sanitized error creation.
+ *
+ * Exports:
+ * - LiferayErrors: factory helpers for domain-specific errors
+ * - LiferayErrorCode: enumeration of all error codes
+ * - sanitizeErrorMessage: message sanitizer (prevents secret leakage)
+ */
+
+export {LiferayErrors, withErrorMetadata, type LiferayErrorOptions} from './liferay-error-factory.js';
+export {LiferayErrorCode, getErrorCodeMetadata} from './liferay-error-codes.js';
+export {sanitizeErrorMessage, sanitizeErrorDetails} from './error-sanitizer.js';
